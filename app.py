de1a033bb8da78ec9abb501f98b380fac4321b88
bouslougui <infobachir@gmail.com>
2021-06-06 20:57:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import os
 from azure.mgmt.resource import SubscriptionClient
 from azure.common.credentials import ServicePrincipalCredentials
 
+from azureml.core.authentication import ServicePrincipalAuthentication
+
+from azureml.core import Workspace
+
 
 app = Flask(__name__)
 
@@ -16,11 +20,21 @@ credential = ServicePrincipalCredentials(tenant=tenant_id, client_id=client_id, 
 subscription_client = SubscriptionClient(credential)
 
 subscription = next(subscription_client.subscriptions.list())
+svc_pr = ServicePrincipalAuthentication(
+    tenant_id=tenant_id,
+    service_principal_id=client_id,
+    service_principal_password=client_secret)
 
+ws = Workspace(
+    subscription_id="9c6e1d8a-238d-4ae2-8b6e-7cb9dbae6faf",
+    resource_group="OCR",
+    workspace_name="P8_OCR",
+    auth=svc_pr
+    )
 
 @app.route("/")
 def hello():
-    return "Welcome to machine learning model APIs!"+subscription.subscription_id
+    return "Welcome to machine learning model APIs!"+ws.location
 
 
 if __name__ == '__main__':
