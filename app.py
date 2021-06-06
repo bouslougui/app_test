c805ed8e9aefce4c72733c443c37762a08fb594b
bouslougui <infobachir@gmail.com>
2021-06-06 20:53:58 +0200

    1

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # Retrieve the IDs and secret to use with ServicePrincipalCredentials
 tenant_id = "e6311692-39bf-4a2b-95d1-2636e4e409c7"
 client_id = "80b5eb4f-f73f-45ee-8e68-47de95d57340"
-client_secret = "fdc98684-b6a8-437b-b89e-21d60a64fdc9"
+client_secret = "DPG~sW85RX8Loh8L4-zc-RCH8GyJq-x5t3"
 
 credential = ServicePrincipalCredentials(tenant=tenant_id, client_id=client_id, secret=client_secret)
 
@@ -20,7 +20,7 @@ subscription = next(subscription_client.subscriptions.list())
 
 @app.route("/")
 def hello():
-    return "Welcome to machine learning model APIs!"
+    return "Welcome to machine learning model APIs!"+subscription.subscription_id
 
 
 if __name__ == '__main__':
